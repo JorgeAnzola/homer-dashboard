28b788d9d26d8f76a8832778d9cd410e66a354a7
Jorge Anzola <jorgeanzola@gmail.com>
2021-08-11 16:21:58 +0200

    Working on development

---
+++ src/components/Service.vue
@@ -0,0 +1,26 @@
+<template>
+  <component v-bind:is="component" :item="item"></component>
+</template>
+
+<script>
+import Generic from "./services/Generic.vue";
+
+export default {
+  name: "Service",
+  components: {
+    Generic,
+  },
+  props: {
+    item: Object,
+  },
+  computed: {
+    component() {
+      const type = this.item.type || "Generic";
+      if (type == "Generic") {
+        return Generic;
+      }
+      return () => import(`./services/${type}.vue`);
+    },
+  },
+};
+</script>
